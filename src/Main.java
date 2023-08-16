29f80cc8b55ce9f0180753900338144acb3f8d03
costa.alessandro <costa.alessandro@ifood.com.br>
2023-08-16 16:09:23 -0300

    Revert "FEAT: Prime numbers class creation and its initialization in main class"
    This reverts commit 2bdb684a9c782036e997acbbe089d44f44e1a420.

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        PrimeNumbers primeNumbers = new PrimeNumbers();
-        Integer size = Integer.parseInt(args[0]);
-        primeNumbers.generateNumbers(size);
-    }
-}
